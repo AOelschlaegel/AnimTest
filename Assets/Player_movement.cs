bc7c94219620a6bd0dad597223ce35d42b0374af
AOelschlaegel <1911+AOelschlaegel@users.noreply.github.com>
2018-08-14 18:36:37 +0200

    unity: extremely basic build
    playermovement, collecting, collision

---
+++ Assets/Player_movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour {
+
+    public int speed = 2;
+
+    public Camera mainCamera;
+
+    private Vector3 scale;
+    private Vector3 scaleFactor;
+    Vector3 mousePos;
+
+    private void Awake()
+    {
+        scaleFactor = new Vector3(0.5f, 0.5f, 0.5f);
+        scale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = Vector2.Lerp(transform.position, mousePos, speed * Time.deltaTime/(scale.x*3));
+        transform.localScale = scale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "enemy" && other.transform.localScale.x < transform.localScale.x)
+        {
+            scale += other.transform.localScale/4;
+            Debug.Log("scale is :" + scale);
+            Destroy(other.gameObject);
+            
+            if(scale.x > 4)
+            {
+                mainCamera.orthographicSize++;
+            }
+
+        }
+    }
+}
---
+++ Assets/SpawnController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+    public GameObject enemy;
+    private int enemies = 0;
+    private int playerCount;
+
+    private void Update()
+    {
+        if (enemies < 10)
+        {
+            for (int i = 0; i < enemies; i++)
+            {
+                GameObject.Instantiate(enemy, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0f), transform.rotation);
+                enemies++;
+            }
+        }
+    }
+}
